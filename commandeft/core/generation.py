8e2e1883ed4dbcd7256c5068f32a63969461dc15
Petros <petros.sidirokastritis@persado.com>
2023-06-20 21:45:43 +0300

    Remove microsoft's guidance and use openai package instead in order to request OpenAI chat completions

--- commandeft/core/generation.py
+++ commandeft/core/generation.py
@@ -1,6 +1,6 @@
 import sys
 import click
-import guidance
+import openai
 
 from commandeft.util.gen_util import (
     get_current_shell,
@@ -9,50 +9,49 @@ from commandeft.util.gen_util import (
 )
 from commandeft.util.config_util import get_configuration
 
+openai.api_key = get_configuration("api_key")
+
 
 def generate_command(user_prompt):
-    api_key = get_configuration("api_key")
-    llm = guidance.llms.OpenAI(
-        get_configuration("model"),
-        api_key=api_key,
-    )
-
     current_shell = get_current_shell()
     current_os = get_current_os()
     temperature = get_configuration("temperature")
     max_tokens = get_configuration("max_tokens")
 
-    template_default = """{{#system~}}
-        Assume the role of a shell scripting expert with the given specifications:
-        Shell: {{current_shell}}
-        OS: {{current_os}}
-        {{~/system}}
-        {{#user~}}
-        Provide only the raw command inside a code block that best fulfills the following request: {{user_prompt}}
-        Prefer oneliners. No other text is allowed.
-        {{~/user}}
-        {{#assistant~}}
-        {{gen 'result' max_tokens=chosen_max_tokens temperature=chosen_temperature}}
-        {{~/assistant}}"""
+    assistane_message = [
+        {
+            "role": "system",
+            "content": f"""
+                Assume the role of a shell scripting expert with the given specifications:
+                Shell: {current_shell}
+                OS: {current_os}
+            """,
+        }
+    ]
 
-    # pylint: disable=not-callable
-    program = guidance(
-        template_default,
-        llm=llm,
+    user_message = [
+        {
+            "role": "user",
+            "content": f"""
+                Provide only the raw command inside a code block that best fulfills the following request: {user_prompt}
+                Prefer oneliners. No other text is allowed.
+            """,
+        }
+    ]
+
+    messages = assistane_message + user_message
+
+    completion = openai.ChatCompletion.create(
+        model=get_configuration("model"),
+        max_tokens=max_tokens + 55,
+        temperature=temperature,
+        messages=messages,
     )
 
-    res = program(
-        user_prompt=user_prompt,
-        chosen_temperature=temperature,
-        current_shell=current_shell,
-        current_os=current_os,
-        # 55 is the number of tokens consumed by the guided prompt
-        chosen_max_tokens=max_tokens + 55,
-        caching=True,
-    )
+    res = completion.choices[0].message.content
     try:
-        command = parse_code_block(res["result"])
+        command = parse_code_block(res)
         return command
     except ValueError:
-        click.echo(click.style(res["result"].replace(". ", ".\n"), fg="red"))
+        click.echo(click.style(res.replace(". ", ".\n"), fg="red"))
         sys.exit(1)
