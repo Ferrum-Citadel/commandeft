b903d1861a8d77500574ac6357fca050e4cd6ea9
GIORGOS SKOURTSIDIS <giorgosskourtsidis@gmail.com>
2023-07-29 21:34:20 +0300

    1st unit test: get_models_list()

---
+++ tests/test_utils.py
@@ -0,0 +1,8 @@
+from commandeft.constants.consts import Models
+import pytest
+
+def test_get_models_list():
+    expected_models = [Models.GPT_3_5_TURBO, Models.GPT_4]
+    models_list = Models.get_models_list()
+    assert isinstance(models_list, list)
+    assert models_list == expected_models
